a77eab37cd8d1e8e7eb8bacb42673d10470b5e7c
Rodian Jasef Saby Sanabria <rjsaby@gmail.com>
2023-01-19 14:39:05 -0500

    Se incorpora la capacidad de leer otro tipo de geometrías en el GeoJSON

--- PGAR_Unificacion_General_Capas.py
+++ PGAR_Unificacion_General_Capas.py
@@ -1,6 +1,10 @@
 import os
 import arcpy
 import pathlib
+import geopandas as gpd
+import fiona
+import pandas as pd
+from arcgis.features import GeoAccessor, GeoSeriesAccessor
 
 # Ruta del directorio donde se localizan los archivos geojson.
 ruta_geojson = arcpy.GetParameterAsText(0)
@@ -20,6 +24,13 @@ lista_ruta_archivo = []
 for rutas in nombre_geojson:
     lista_ruta_archivo.append(os.path.join(ruta_geojson, rutas))
     
+# ? Diccionario que se usará en la transformación del geoJSON a Feature Class
+geometrias = {'Polygon':'POLYGON',
+              'MultiPolygon':'POLYGON',
+              'LineString':'POLYLINE',
+              'MultiLineString':'POLYLINE',
+              'Point':'POINT'}
+
 for generacion_feature in lista_ruta_archivo:
     if pathlib.Path(generacion_feature).suffix == '.geojson':
         nombre_geojson = os.path.split(generacion_feature)
@@ -28,8 +39,19 @@ for generacion_feature in lista_ruta_archivo:
         nombre_geojson_parametrizado = (nombre_geojson[1].split(".")[0]).replace("%","_")
         if nombre_geojson_parametrizado != os.path.basename(ruta_geojson):
             salida_feature = os.path.join(arcpy.env.workspace, nombre_geojson_parametrizado)
-            arcpy.conversion.JSONToFeatures(generacion_feature, salida_feature, 'POLYGON')
-            print("geojson {0} convertido al feature {1}".format(nombre_geojson, nombre_geojson_parametrizado))
+            # ? Se exporta el geoJSON a GeoDataFrame
+            df = gpd.read_file(generacion_feature)
+            # ? Por medio del atributo .geom_type se extrae el tipo de geometría para cada registro
+            tipo_geometria = df.geom_type
+            # ? Como las geometrias varías si son simples o multiples, se seleccina la que se almacena en la primera fila
+            primer_registro_tipo_geometria = tipo_geometria.loc[0]
+            # ? Se recorre el diccionario
+            for valor, llave in geometrias.items():
+                # ? Se compara la geometría seleccionada con el valor almacenado en el diccionario
+                if primer_registro_tipo_geometria == valor:
+                    # ? Se genera el feature class, tomando como variable el valor de la geometría
+                    arcpy.conversion.JSONToFeatures(generacion_feature, salida_feature, llave)
+                    print("geojson {0} convertido al feature {1}".format(nombre_geojson, nombre_geojson_parametrizado))
     elif pathlib.Path(generacion_feature).suffix == '.json':
         nombre_geojson = os.path.split(generacion_feature)
         nombre_geojson_parametrizado = (nombre_geojson[1].split(".")[0]).replace(" ","_")
@@ -37,10 +59,21 @@ for generacion_feature in lista_ruta_archivo:
         nombre_geojson_parametrizado = (nombre_geojson[1].split(".")[0]).replace("%","_")
         if nombre_geojson_parametrizado != os.path.basename(ruta_geojson):
             salida_feature = os.path.join(arcpy.env.workspace, nombre_geojson_parametrizado)
-            arcpy.conversion.JSONToFeatures(generacion_feature, salida_feature, 'POLYGON')
-            print("json {0} convertido al feature {1}".format(nombre_geojson, nombre_geojson_parametrizado))    
+            # ? Se exporta el geoJSON a GeoDataFrame
+            df = gpd.read_file(generacion_feature)
+            # ? Por medio del atributo .geom_type se extrae el tipo de geometría para cada registro
+            tipo_geometria = df.geom_type
+            # ? Como las geometrias varías si son simples o multiples, se seleccina la que se almacena en la primera fila
+            primer_registro_tipo_geometria = tipo_geometria.loc[0]
+            # ? Se recorre el diccionario
+            for valor, llave in geometrias.items():
+                # ? Se compara la geometría seleccionada con el valor almacenado en el diccionario
+                if primer_registro_tipo_geometria == valor:
+                    # ? Se genera el feature class, tomando como variable el valor de la geometría
+                    arcpy.conversion.JSONToFeatures(generacion_feature, salida_feature, llave)
+                    print("json {0} convertido al feature {1}".format(nombre_geojson, nombre_geojson_parametrizado))
     else:
-        print("Es la base de datos")  
+        print("Es la base de datos") 
         
 lista_capas_convertidas = arcpy.ListFeatureClasses()
 nombre_campo = 'nombre_capa'
